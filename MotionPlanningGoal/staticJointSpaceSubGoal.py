846c3542c98e0c23fca008daeff1a424ad4db750
Max <m.spahn@tudelft.nl>
2022-01-21 11:44:17 +0100

    Adds Joint space goals.

---
+++ MotionPlanningGoal/staticJointSpaceSubGoal.py
@@ -0,0 +1,53 @@
+import numpy as np
+from MotionPlanningGoal.subGoal import SubGoal
+from MotionPlanningSceneHelpers.motionPlanningComponent import DimensionNotSuitableForEnv
+
+class JointSpaceGoalsNotSupportedError(Exception):
+    pass
+
+
+class StaticJointSpaceSubGoal(SubGoal):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._required_keys = [
+            "m",
+            "w",
+            "prime",
+            "epsilon",
+            "type",
+            "desired_position",
+            "indices",
+        ]
+        self.checkCompleteness()
+        self.checkDimensionality()
+
+    def limitLow(self):
+        if 'low' in self._contentDict:
+            return np.array(self._contentDict['low'])
+        else:
+            return np.ones(self.m()) * -1
+
+    def limitHigh(self):
+        if 'high' in self._contentDict:
+            return np.array(self._contentDict['high'])
+        else:
+            return np.ones(self.m()) * 1
+
+    def evaluate(self, **kwargs):
+        return []
+
+    def toDict(self):
+        return self._contentDict
+
+    def position(self, **kwargs):
+        return self._contentDict['desired_position']
+
+    def shuffle(self):
+        randomPos = np.random.uniform(self.limitLow(), self.limitHigh(), self.m())
+        self._contentDict['desired_position'] = randomPos.tolist()
+
+    def renderGym(self, viewer, **kwargs):
+        raise JointSpaceGoalsNotSupportedError()
+
+    def add2Bullet(self, pybullet):
+        raise JointSpaceGoalsNotSupportedError()
--- tests/unittests/test_subGoalCreator.py
+++ tests/unittests/test_subGoalCreator.py
@@ -1,4 +1,5 @@
 import pytest
+from MotionPlanningGoal.staticJointSpaceSubGoal import StaticJointSpaceSubGoal
 from MotionPlanningGoal.subGoalCreator import SubGoalCreator, UnknownSubGoalType
 
 def test_subGoalCreator():
@@ -6,6 +7,13 @@ def test_subGoalCreator():
     subGoalCreator = SubGoalCreator()
     subgoal0 = subGoalCreator.createSubGoal('staticSubGoal', 'subgoal0', goalDict)
     assert subgoal0.indices() == [0, 1]
+
+def test_subGoalCreator_jointSpaceGoal():
+    goalDict = {'m': 2, 'w': 5.0, 'prime': True, 'indices': [0, 1], 'desired_position': [0.01, 0.2], 'epsilon': 0.2, 'type': 'staticJointSpaceSubGoal'}
+    subGoalCreator = SubGoalCreator()
+    subgoal0 = subGoalCreator.createSubGoal('staticJointSpaceSubGoal', 'subgoal0', goalDict)
+    assert subgoal0.type() == "staticJointSpaceSubGoal"
+    assert isinstance(subgoal0, StaticJointSpaceSubGoal)
 
 def test_unknownSubGoalTypeError():
     goalDict = {'m': 2, 'w': 5.0, 'prime': True, 'indices': [0, 1], 'parent_link': 0, 'child_link': 3, 'desired_position': [0.01, 0.2], 'epsilon': 0.2, 'type': 'staticSubGoal'}
