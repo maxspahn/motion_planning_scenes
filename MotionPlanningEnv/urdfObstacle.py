b0db374a9b3de4580f8acede205a482dd6755ac1
Gijs Groote <gijsgroote@hotmail.com>
2022-08-26 14:32:23 +0200

    remove geometry and other redundant functions for urdfObstacle

--- MotionPlanningEnv/urdfObstacle.py
+++ MotionPlanningEnv/urdfObstacle.py
@@ -1,4 +1,3 @@
-
 from MotionPlanningEnv.collisionObstacle import (
     CollisionObstacle,
     CollisionObstacleConfig,
@@ -6,50 +5,21 @@ from MotionPlanningEnv.collisionObstacle import (
 from MotionPlanningSceneHelpers.motionPlanningComponent import (
     DimensionNotSuitableForEnv,
 )
-import numpy as np
 
 from dataclasses import dataclass
 from omegaconf import OmegaConf
-from typing import List, Optional
-
-
-@dataclass
-class GeometryConfig:
-    """Configuration dataclass for geometry for urdf obstacles.
-
-    This configuration class holds information about position.
-
-    Parameters:
-    ------------
-
-    position: list: Position of the obstacle
-    """
-
-    position: List[float]
-
 
 @dataclass
 class UrdfObstacleConfig(CollisionObstacleConfig):
-    """Configuration dataclass for sphere obstacle.
-
-    This configuration class holds information about the position, size
-    and randomization of a spherical obstacle.
+    """
+    This configuration class holds information about the urdf obstacle.
 
     Parameters:
     ------------
-
     urdf : str : Filename of the urdf
-    geometry : GeometryConfig : Geometry of the obstacle
-    low: GeometryConfig : Lower limit for randomization
-    high: GeometryConfig : Upper limit for randomization
 
     """
-
     urdf: str
-    geometry: GeometryConfig
-    low: Optional[GeometryConfig] = None
-    high: Optional[GeometryConfig] = None
-
 
 class UrdfObstacle(CollisionObstacle):
     def __init__(self, **kwargs):
@@ -59,18 +29,6 @@ class UrdfObstacle(CollisionObstacle):
 
     def urdf(self):
         return self._config.urdf
-
-    def position(self):
-        return self._config.geometry.position
-
-    def velocity(self):
-        return np.zeros(3)
-
-    def acceleration(self):
-        return np.zeros(3)
-
-    def dimension(self):
-        return len(self._config.geometry.position)
 
     def add_to_bullet(self, pybullet):
         if self.dimension() != 3:
