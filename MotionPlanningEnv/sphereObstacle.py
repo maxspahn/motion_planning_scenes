c1e4ea4f2b2883329ba638f99d4c255a1f9edb7f
Gijs Groote <gijsgroote@hotmail.com>
2022-08-26 19:12:54 +0200

    move position from sphereObstacle to collisionObstacle

--- MotionPlanningEnv/sphereObstacle.py
+++ MotionPlanningEnv/sphereObstacle.py
@@ -1,73 +1,71 @@
-from dataclasses import dataclass
-import numpy as np
-import os
+from dataclasses import dataclass, field
 import csv
+from typing import List, Optional
+import numpy as np
+
+import os
+from omegaconf import OmegaConf
+
+from MotionPlanningSceneHelpers.motionPlanningComponent import (
+    ComponentIncompleteError,
+    DimensionNotSuitableForEnv,
+)
 from MotionPlanningEnv.collisionObstacle import (
     CollisionObstacle,
     CollisionObstacleConfig,
 )
-from MotionPlanningSceneHelpers.motionPlanningComponent import (
-    DimensionNotSuitableForEnv,
-)
-
-from omegaconf import OmegaConf
-from typing import List, Optional
-
-
-class SphereObstacleMissmatchDimensionError(Exception):
-    pass
 
 
 @dataclass
 class GeometryConfig:
-    """Configuration dataclass for geometry.
-
-    This configuration class holds information about position
-    and radius of a sphere obstacle.
-
+    """
+    Configuration dataclass for geometry.
+    This configuration class holds information about
+    the geometry of the sphere obstacle.
     Parameters:
     ------------
-
-    position: list: Position of the obstacle
     radius: float: Radius of the obstacle
     """
-
-    position: List[float]
     radius: float
-
 
 @dataclass
 class SphereObstacleConfig(CollisionObstacleConfig):
-    """Configuration dataclass for sphere obstacle.
-
-    This configuration class holds information about the position, size
-    and randomization of a spherical obstacle.
-
+    """
+    Configuration dataclass for sphere obstacle.
     Parameters:
     ------------
-
     geometry : GeometryConfig : Geometry of the obstacle
+    orientation: list: Orientation of the obstacle
     movable : bool : Flag indicating whether an obstacle can be pushed around
+    mass: float : Mass of the object, only used if movable set to true
+    color : list : [r,g,b,a] rgba Color where r,g,b,a are floats between 0 and 1
     low : GeometryConfig : Lower limit for randomization
     high : GeometryConfig : Upper limit for randomization
     """
-
     geometry: GeometryConfig
+    orientation: List[float] = field(default_factory=list)
     movable: bool = False
+    mass: float = 1
+    color: List[float] = field(default_factory=list)
     low: Optional[GeometryConfig] = None
     high: Optional[GeometryConfig] = None
 
-
 class SphereObstacle(CollisionObstacle):
+    """
+    Sphere obstacle class.
+    """
     def __init__(self, **kwargs):
         schema = OmegaConf.structured(SphereObstacleConfig)
         super().__init__(schema, **kwargs)
-        self.check_completeness()
+        self._geometry_keys = ["radius"]
 
-    def dimension(self):
-        return len(self._config.geometry.position)
+        self.check_completeness()
+        self.check_geometry_completeness()
 
     def limit_low(self):
+        """
+        Returns lowest limit.
+        """
         if self._config.low:
             return [
                 np.array(self._config.low.position),
@@ -77,6 +75,9 @@ class SphereObstacle(CollisionObstacle):
             return [np.ones(self.dimension()) * -1, 0]
 
     def limit_high(self):
+        """
+        Returns highest limit.
+        """
         if self._config.high:
             return [
                 np.array(self._config.high.position),
@@ -85,79 +86,85 @@ class SphereObstacle(CollisionObstacle):
         else:
             return [np.ones(self.dimension()) * 1, 1]
 
-    def position(self, **kwargs):
-        return self._config.geometry.position
-
-    def velocity(self, **kwargs):
-        return np.zeros(self.dimension())
-
-    def acceleration(self, **kwargs):
-        return np.zeros(self.dimension())
-
     def radius(self):
+        """
+        Return objecs radius.
+        """
         return self._config.geometry.radius
 
     def shuffle(self):
+        """
+        Set new position and radius for object.
+        """
         random_pos = np.random.uniform(
             self.limit_low()[0], self.limit_high()[0], self.dimension()
         )
         random_radius = np.random.uniform(
             self.limit_low()[1], self.limit_high()[1], 1
         )
-        self._config.geometry.position = random_pos.tolist()
+        self._config.position = random_pos.tolist()
         self._config.geometry.radius = float(random_radius)
 
     def movable(self):
+        """
+        Indicates it the object is movable.
+        """
         return self._config.movable
 
     def csv(self, file_name, samples=100):
+        """
+        Save as CSV file.
+        """
         theta = np.arange(-np.pi, np.pi + np.pi / samples, step=np.pi / samples)
-        x = self.position()[0] + (self.radius() - 0.1) * np.cos(theta)
-        y = self.position()[1] + (self.radius() - 0.1) * np.sin(theta)
+        x_pos = self.position()[0] + (self.radius() - 0.1) * np.cos(theta)
+        y_pos = self.position()[1] + (self.radius() - 0.1) * np.sin(theta)
         with open(file_name, mode="w") as file:
             csv_writer = csv.writer(file, delimiter=",")
             for i in range(2 * samples):
-                csv_writer.writerow([x[i], y[i]])
+                csv_writer.writerow([x_pos[i], y_pos[i]])
 
-    def render_gym(self, viewer, rendering, **kwargs):
+    def render_gym(self, viewer, rendering):
+        """
+        Render object in gym environment.
+        """
         if self.dimension() != 2:
             raise DimensionNotSuitableForEnv(
                 "PlanarGym only supports two dimensional obstacles"
             )
-        x = self.position()
-        tf = rendering.Transform(rotation=0, translation=(x[0], x[1]))
+        x_pos = self.position()
         joint = viewer.draw_circle(self.radius())
-        joint.add_attr(tf)
+        joint.add_attr(rendering.Transform(
+            rotation=0, translation=(x_pos[0], x_pos[1])))
 
     def add_to_bullet(self, pybullet):
+        """
+        Adds object to pybullet environment.
+        """
+        pybullet.setAdditionalSearchPath(
+                os.path.dirname(os.path.realpath(__file__)))
+
+        collision_shape = pybullet.createCollisionShape(
+                pybullet.GEOM_SPHERE, radius=self.radius())
+
+        visual_shape_id = pybullet.createVisualShape(
+            pybullet.GEOM_MESH,
+            fileName="sphere.obj",
+            rgbaColor=self.color(),
+            meshScale=[self.radius(), self.radius(), self.radius()]
+        )
+
         if self.dimension() == 2:
             base_position = self.position() + [0.0]
         elif self.dimension() == 3:
             base_position = self.position()
         else:
             raise DimensionNotSuitableForEnv(
-                "Pybullet only supports three dimensional obstacles"
-            )
-        collision_shape = pybullet.createCollisionShape(
-            pybullet.GEOM_SPHERE, radius=self.radius()
-        )
-        visual_shape_id = -1
-        base_orientation = [0, 0, 0, 1]
-        mass = int(self.movable())
-        pybullet.setAdditionalSearchPath(
-            os.path.dirname(os.path.realpath(__file__))
-        )
-        visual_shape_id = pybullet.createVisualShape(
-            pybullet.GEOM_MESH,
-            fileName="sphere_smooth.obj",
-            rgbaColor=[1.0, 0.0, 0.0, 1.0],
-            specularColor=[1.0, 0.5, 0.5],
-            meshScale=[self.radius(), self.radius(), self.radius()],
-        )
-        pybullet.createMultiBody(
-            mass,
-            collision_shape,
-            visual_shape_id,
-            base_position,
-            base_orientation,
-        )
+                "Pybullet only supports three dimensional obstacles")
+
+        base_orientation = self.orientation()
+
+        pybullet.createMultiBody(self._config.mass,
+              collision_shape,
+              visual_shape_id,
+              base_position,
+              base_orientation)
