0df5596b154537e106901de2d2872512e70653ce
Max Spahn <m.spahn@tudelft.nl>
2022-10-12 16:38:08 +0200

    Adds evaluate components function.

--- tests/unittests/test_sphereObstacle.py
+++ tests/unittests/test_sphereObstacle.py
@@ -35,6 +35,19 @@ def test_errorRaiseIncompleteDict():
         sphere_obst= SphereObstacle(name='simpleSphere', content_dict=obst_dict)
         sphere_obst.radius()
 
+def test_mask_selection():
+    obst_dict= {'type': 'sphere', 'geometry': {'position': [0.1, 0.2, 0.4], 'radius': 0.2}}
+    sphere_obst = SphereObstacle(name='simpleSphere', content_dict=obst_dict)
+    mask = ["type", "position", "radius"]
+    selected_items = sphere_obst.evaluate_components(mask, 0)
+    assert isinstance(selected_items, dict)
+    assert selected_items['type'] == 'sphere'
+    assert selected_items['position'][0] == 0.1
+    assert selected_items['position'][1] == 0.2
+    assert selected_items['position'][2] == 0.4
+    assert list(selected_items.keys()) == ["type", "position", "radius"]
+
+
 def test_saving_obstacle():
     obst_dict= {'type': 'sphere', 'geometry': {'position': [0.1, 0.2, 0.4], 'radius': 0.2}}
     sphere_obst = SphereObstacle(name='simpleSphere', content_dict=obst_dict)
