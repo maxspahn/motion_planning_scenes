fix[distance]: Adds optional argument for time for distance calculation.

mpscenes/obstacles/collision_obstacle.py
@@ -122,9 +122,13 @@ class CollisionObstacle(MPComponent):
     def movable(self) -> bool:
         return self._config.movable
 
-    def position_into_obstacle_frame(self, positions: np.ndarray) -> np.ndarray:
+    def position_into_obstacle_frame(self, positions: np.ndarray, **kwargs) -> np.ndarray:
+        if 't' in kwargs:
+            t = kwargs.get('t')
+        else:
+            t = 0
         transformation_matrix = quaternion_to_homogeneous_matrix(
-            self.position(), self.orientation()
+            self.position(t=t), self.orientation()
         )
         if len(positions.shape) > 1:
             nb_points = positions.shape[0]

mpscenes/obstacles/sphere_obstacle.py
@@ -98,6 +98,6 @@ class SphereObstacle(CollisionObstacle):
             for i in range(2 * samples):
                 csv_writer.writerow([x[i], y[i]])
 
-    def distance(self, position: np.ndarray) -> float:
-        pos = self.position_into_obstacle_frame(position)
+    def distance(self, position: np.ndarray, **kwargs) -> float:
+        pos = self.position_into_obstacle_frame(position, **kwargs)
         return np.linalg.norm(pos, axis=0) - self.radius()